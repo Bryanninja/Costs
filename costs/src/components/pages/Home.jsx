de702a9fd0781346765503e58fbe47c988a068d2
Álex Bryan <bryannascimentopl800@gmail.com>
2025-08-03 17:09:26 -0300

    Remover node_modules rastreados e garantir que estão no .gitignore

--- costs/src/components/pages/Home.jsx
+++ costs/src/components/pages/Home.jsx
@@ -1,9 +1,15 @@
+import './Home.css'
+import savings from '../../assets/img/savings.svg'
+import LinkButton from '../layout/LinkButton';
+
 function Home(){
     return(
-        <div>
-            <h1>Home</h1>
-            
-        </div>
+        <section className='home_container'>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar projeto"/>
+            <img src={savings} alt="Costs"/>
+        </section>
     )
 }
 
